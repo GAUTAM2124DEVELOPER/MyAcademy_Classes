7d5793bf38fcf1a81a70dfae9a1a9bdbf5a41f81
GAUTAM2124DEVELOPER <90195585+GAUTAM2124DEVELOPER@users.noreply.github.com>
2022-04-04 05:59:34 +0530

    Add files via upload

---
+++ SCRIPT/script.js
@@ -0,0 +1,14 @@
+// let menu=document.getElementsByClassName("icon-line")
+// let nav=document.getElementsByClassName("navul")
+// menu.addEventListener('click',()=>{
+//     console.log(dbjasdj);
+//     nav.classList.toggle('navul')
+// })
+// let navline = document.querySelectorAll('.nav-line')
+// let navbar=document.querySelectorAll('.navbar')
+// let navul=document.querySelectorAll('.navul')
+// navline.addEvenListener('click',()=>{
+//     navul.classList.toggle('v-class-resp');
+//     navbar.classList.toggle('h-nav-resp');
+// })
+
